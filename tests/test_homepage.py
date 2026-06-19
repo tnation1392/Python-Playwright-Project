d76344d21f5a4daee18d18c51c678feacb1924c3
tnation1392 <96146191+tnation1392@users.noreply.github.com>
2026-06-19 14:46:39 -0400

    Updated conftest.py to centralize base URL

--- tests/test_homepage.py
+++ tests/test_homepage.py
@@ -1,7 +1,5 @@
 from pages.home_page import HomePage
 
 
-def test_homepage_loads(page):
-    home = HomePage(page)
-    home.goto()
-    home.assert_loaded()
+def test_homepage_loads(home_page):
+    home_page.assert_loaded()
